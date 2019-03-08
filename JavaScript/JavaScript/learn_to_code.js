28b07d26f9d81b3b3172daca6e68253e07505464
nisha <nishavdc@gmail.com>
2019-03-08 19:47:06 +0530

    added 6 programs. Initial commit

---
+++ JavaScript/JavaScript/learn_to_code.js
@@ -0,0 +1,41 @@
+//shift +tab -allign
+
+
+//var name = "jack";
+//var age = 23;
+//var message = "Hi my name is" + name + " and i am " + age + "years old";
+//console.log(message);
+//var sum = 10 + 15;
+//var sub = 10 - 15;
+//var mul = 10 * 3;
+//var div = 10 / 3 ;
+//var mod = 10 % 3;
+//var message = "10/3 =3 with a remainder of " + mod;
+//console.log(message);
+////console.log(div);
+//var myaccountbalance= 300;
+//var nikeSBshoes = 600.23;
+//var coupon = 400;
+//if (nikeSBshoes<=myaccountbalance)
+//    {
+//        myaccountbalance-=nikeSBshoes;
+//        console.log("We just bought some dope shoes");
+//        console.log("Account balance " + myaccountbalance);
+//    }
+//else if(nikeSBshoes-coupon <=myaccountbalance)
+//    {
+//        console.log("We just bought some dope shoes with a coupon");
+//        myaccountbalance-=nikeSBshoes-coupon;
+//        console.log("Account balance " + myaccountbalance);
+//     
+//    }
+//else{
+//       console.log("your account balance is low");
+//}
+var student ={
+    firstname:"john",
+    lastname :"Parker",
+    age:7
+};
+console.log(student.firstname);
+console.log (student.lastname);
